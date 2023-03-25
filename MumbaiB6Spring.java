b3e77d6c63bf1360b5eefb49abb660dab04beff8
Manoj0107 <127963848+ManojK0107@users.noreply.github.com>
2023-03-25 17:50:01 +0530

    Add files via upload

---
+++ MumbaiB6Spring.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumbaiB6Spring {
+    public static void main(String[] args) {
+        SpringApplication.run(MumbaiB6Spring.class,args);
+    }
+}
